8f7250fcf16dbd11df278ca6301ba4d316934b3c
zawhtetnaing10 <zawgyi.gog@gmail.com>
2024-12-09 22:17:49 +0630

    AppBar completed. Status bar color and status bar icon color changed

--- app/src/main/java/com/zg/netflixloginscreenjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/zg/netflixloginscreenjetpackcompose/MainActivity.kt
@@ -1,29 +1,49 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.zg.netflixloginscreenjetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zg.netflixloginscreenjetpackcompose.ui.theme.Black
 import com.zg.netflixloginscreenjetpackcompose.ui.theme.NetflixLoginScreenJetpackComposeTheme
+import com.zg.netflixloginscreenjetpackcompose.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(android.graphics.Color.BLACK),
+            navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.BLACK)
+        )
         setContent {
             NetflixLoginScreenJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NetflixLoginScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NetflixLoginScreen()
             }
         }
     }
@@ -31,7 +51,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NetflixLoginScreen(modifier: Modifier = Modifier) {
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = Black),
+            title = {
+                Image(
+                    painterResource(R.drawable.netflix_logo_app_bar),
+                    contentDescription = "Netflix AppBar Title"
+                )
+            },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                    contentDescription = null,
+                    tint = White,
+                    modifier = Modifier.size(32.dp)
+                )
+            },
+            actions = {
+                Text(
+                    "Help",
+                    style = TextStyle(color = White, fontWeight = FontWeight.Bold, fontSize = 16.sp),
+                    modifier = Modifier.padding(end = 12.dp)
+                )
+            }
+        )
+    }, modifier = modifier.fillMaxSize()) { innerPadding ->
+        NetflixLoginContent(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
+@Composable
+fun NetflixLoginContent(modifier: Modifier = Modifier) {
+    Surface(color = Black, modifier = modifier.fillMaxSize()) {
+        Box(contentAlignment = Alignment.Center) {
+            Text("Netflix")
+        }
+    }
 }
 
 
--- app/src/main/java/com/zg/netflixloginscreenjetpackcompose/ui/theme/Color.kt
+++ app/src/main/java/com/zg/netflixloginscreenjetpackcompose/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val Black = Color(red = 0, green = 0, blue = 0)
 val NetflixRed = Color(red = 83, green = 14, blue = 13)
 val NetflixGrey = Color(red = 50, green = 50, blue = 50)
+val White = Color(red = 255, green = 255, blue = 255)
